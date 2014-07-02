ae0326f2aa6c27220326d8dd58dc70306eee1d2c
olia0907 <olia0907>
2014-07-01 20:46:07 -0400

    find insertion

--- FindInsertion.java
+++ FindInsertion.java
@@ -7,4 +7,37 @@ public class FindInsertion {
 
 	}
 
+    public int searchInsert(int[] A, int target) {
+        if( A== null) return 0 ;
+        
+        int length = A.length ;
+        
+        if( length == 1 )
+        {
+            if(A[0] < target) 
+                return 1;
+            return 0 ;
+        }
+        else
+        //if(length >= 2)
+        {
+          int pre, post ;
+          pre = 0;  
+          post = 1;
+          while( post < length )
+          {
+              if(A[pre] > target) 
+                { return pre ; }
+              else 
+                if( A[pre] < target && A[post] > target )
+                {return post ;}
+                else
+                  {
+                      post++;
+                      pre++;
+                  };
+          };
+          return post ;
+        }
+    }
 }
